b538c8518fa12f445c316c50d9ae050333d26fb5
parkertheplank <77869681+parkertheplank@users.noreply.github.com>
2022-09-26 23:26:14 -0500

    worked on basic feedback loop

--- masterSAM/masterSAM.ino
+++ masterSAM/masterSAM.ino
@@ -15,20 +15,20 @@
 #define startButton 3
 #define vibButton 5
 #define lcd 4
-#define 
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pneumSetup();
 }
 
 
 //----------CONTROL FLOW FUNCTIONS------------------------
 void loop() {
   // put your main code here, to run repeatedly:
-
+  feedbackLoop(14.5);
 }
-void main(){}
+/*void main(){}
 void airMaster() {}
 
 
@@ -37,12 +37,7 @@ void vibrate() {} // to vibrate the SAM.
 void waterFill() {} //Turn on water pump and water valves to begin filling the SAM with water for 10 seconds.
 void tilt() {} // Tilt the SAM to allow the air bubbles to exit.
 void waterClose(){} //After 10 seconds, shut off water pump and close water valves.
-void airPressurize(){} //Turn on air pump and two-way air valve to begin pressurizing the SAM.
-void airRead() {} //Read the pressure sensor analog output to match 14.5 psi. 
-void airHalt() {} //raise air pressure to desired value
-void airBleed() {} //Decrease air pressure to desired value
-void airLever() {} //Push top lever to release pressure into the chamber for 10-20 seconds to allow the pressure to reach equilibrium. 
 void airUSBwrite() {} //Record the air pressure data.
-
+*/
 
 //--------DISPLAY FUNCTIONS-------------------------------
