5ddb94adec9e22e1b41d49455c7b3bc9fd12bfcc
parkertheplank <77869681+parkertheplank@users.noreply.github.com>
2022-10-18 15:56:04 -0500

    update

--- unittestADC/unittestADC.ino
+++ unittestADC/unittestADC.ino
@@ -40,13 +40,12 @@ void loop(void)
   adc0 = ads.readADC_SingleEnded(0);
   volts0 = adc0 * .0001875;
   //volts0 = ads.computeVolts(adc0);
-  volts0 = volts0 - .005;
   //psi= (volts0-0.99595617)/0.066932271;
   //psi= (14.940378*volts0)-14.88005943;
   psi= (14.991578*volts0)-15.00005943;
   Serial.println("-----------------------------------------------------------");
   Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(volts0,4); Serial.println("V");
-  Serial.print("psi: "); Serial.println(psi);
+  Serial.print("psi: "); Serial.println(psi,4);
   
 
   delay(1000);
